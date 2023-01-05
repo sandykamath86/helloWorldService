2a77fd7a8bba70c57860ada3cdc287cbf250845b
Sandeep Kamath <skamath@netapp.com>
2023-01-05 10:30:11 +0530

    first commit

---
+++ src/main/java/com/netapp/helloWorldService/HelloWorldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.netapp.helloWorldService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netapp/helloWorldService/HelloWorldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netapp.helloWorldService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
